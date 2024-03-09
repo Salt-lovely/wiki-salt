563cadd72b8740871b77d869815cd69685988a75
Salt <732237136@qq.com>
2024-03-10 02:57:25 +0800

    doc: 预备升级1.0.1

--- src/index.ts
+++ src/index.ts
@@ -18,24 +18,22 @@ import { model } from './model'
 /*!
 Copyright (c) 2022 Salt-lovely
 wiki-salt is licensed under Mulan PSL v2.
-You can use this software according to the terms and conditions of the Mulan PSL v2. 
+You can use this software according to the terms and conditions of the Mulan PSL v2.
 You may obtain a copy of Mulan PSL v2 at:
-        http://license.coscl.org.cn/MulanPSL2 
-THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.  
-See the Mulan PSL v2 for more details.  
+        http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
 */
 /*!
 // ==UserScript==
 // @name         WikiSalt编辑工具
 // @namespace    salt.is.lovely.wiki
-// @version      1.0.0
+// @version      1.0.1
 // @description  WikiSalt编辑工具
 // @author       Salt
-// @match        https://mcbbs-wiki.cn/index.php?*
-// @match        https://mcbbs-wiki.cn/wiki/*
 // @match        https://mcbbs.wiki/index.php?*
 // @match        https://mcbbs.wiki/wiki/*
-// @match        https://wiki.biligame.com/mcplayer/*
+// @match        https://wiki.biligame.com/*
 // @grant        none
 // ==/UserScript==
 */
