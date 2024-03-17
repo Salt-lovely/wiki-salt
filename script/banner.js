24da81adde313c3e2efe3064aa16553b9e05c9ec
Salt <732237136@qq.com>
2024-03-17 22:30:55 +0800

    doc: 修改提示

--- script/banner.js
+++ script/banner.js
@@ -5,7 +5,7 @@ module.exports = {
 // @name         WikiSalt编辑工具
 // @namespace    salt.is.lovely.wiki
 // @version      1.0.1
-// @description  WikiSalt编辑工具
+// @description  WikiSalt编辑工具，需要MediaWiki版本1.28以上，浏览器支持ES2017
 // @author       Salt
 // @match        https://mcbbs.wiki/*
 // @match        https://wiki.biligame.com/*
